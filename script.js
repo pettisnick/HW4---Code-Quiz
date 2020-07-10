4478ed6966a2b7f273917e957538e53d28d48a34
Nick Pettis <pettisnick@gmail.com>
2020-07-09 23:39:02 -0400

    style

--- script.js
+++ script.js
@@ -1,71 +1,51 @@
 var questions = [{
         question: "1. What would be the resule of 3 + 2 * 7?",
-        option1: "35",
-        option2: "327",
-        option3: "17",
+        choices: ["35", "327", "17"],
         answer: "2"
     }, 
     {
         question: "2. Which HTML tag is used to define an interval style sheet?",
-        option1: "<style>",
-        option2: "<script>",
-        option3: "<css>",
+        choices: ["style", "script", "css"],
         answer: "1"
     },
     {
         question: "3. What will this output? var a = [1, 2, 3]; console.log(a[6]);",
-        option1: "undefined",
-        option2: "0",
-        option3: "6",
+        choices: ["undefined", "0", "6"],
         answer: "0"
     }, 
     {
         question: "4. What type of question gives a True/False or Yes/No answer?",
-        option1: "confirm",
-        option2: "prompt",
-        option3: "neither",
+        choices: ["confirm", "prompt", "neither"],
         answer: "0"
     }, 
     {
         question: "5. Out of the 3 fundamental programming languages, Javascript is which one?",
-        option1: "first",
-        option2: "second",
-        option3: "third",
+        choices: ["first", "second", "third"],
         answer: "2"
     }, 
     {
         question: "6. What does querySelector do?",
-        option1: "gives array of any element it can find",
-        option2: "pulls first element it finds",
-        option3: "selects every function it finds",
+        choices: ["gives array of any element it can find", "pulls first element it finds", "selects every function it finds"],
         answer: "1"
     }, 
     {
         question: "7. What does an alert do?",
-        option1: "displays discreetly to the debugger",
-        option2: "displays a pop-up message to the user",
-        option3: "calls 911",
+        choices: ["displays discreetly to the debugger", "displays a pop-up message to the user", "calls 911"],
         answer: "1"
     }, 
     {
         question: "8. What is the method for converting a string to lowercase letters?",
-        option1: ".toLowerCase()",
-        option2: ".Lowercase()",
-        option3: ".toMakeLowerCase()",
+        choices: [".toLowerCase()", ".Lowercase()", ".toMakeLowerCase()"],
         answer: "0"
     }, 
     {
         question: "9. What does the 'i < array.length;' stand for in a for loop?",
-        option1: "iterator",
-        option2: "condition",
-        option3: "increment",
+        choices: ["iterator", "condition", "increment"],
         answer: "1"
     }, 
     {
         question: "10. What function returns the largest integer less than or equal to a given number?",
-        option1: "Math.ceiling()",
-        option2: "Math.floor()",
-        option3: "Math.solid()",
+        choices: ["Math.ceiling()", "Math.floor()", "Math.solid()"],
         answer: "1"
     }];
 
@@ -161,13 +141,13 @@ var questions = [{
 
     //Deduct 15 seconds from time if user chooses wrong answer
     function incorrect() {
-        timeRemaining -= 15;
+      timeRemaining -= 15;
         next();
         
     }
 
     //Increases score by 10 points if correct answer is choosen
-    function corrrect() {
+    function correct() {
         score += 10;
         next();
         
